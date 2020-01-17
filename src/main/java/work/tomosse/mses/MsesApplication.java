f0c5696510e3a80add8bb87c6e9d0eb4ac2f6c7a
shiotomo <t.shiota16@gmail.com>
2020-01-18 04:52:19 +0900

    Spring Boot projectを立ち上げた

---
+++ src/main/java/work/tomosse/mses/MsesApplication.java
@@ -0,0 +1,13 @@
+package work.tomosse.mses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/tomosse/mses/MsesApplicationTests.java
@@ -0,0 +1,13 @@
+package work.tomosse.mses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
