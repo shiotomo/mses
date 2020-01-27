33d039833138ae3b6d461be6473759b2e77de8f5
shiotomo <t.shiota16@gmail.com>
2020-01-27 14:47:21 +0900

    login_logテーブルを作成した

---
+++ src/main/java/work/tomosse/mses/service/LoginLogService.java
@@ -0,0 +1,19 @@
+package work.tomosse.mses.service;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginLogService {
+
+    /**
+     * msesにログインした情報を記録する
+     *
+     * @param principal
+     */
+    public void loginMses(final Principal principal) {
+
+    }
+
+}
